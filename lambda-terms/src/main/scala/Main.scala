2b2ddb34eed28810e43d9121bb48fe6b4bde6abc
NikolayDPaev <niki.paev@gmail.com>
2022-03-27 18:07:12 +0300

    Examples added

--- lambda-terms/src/main/scala/Main.scala
+++ lambda-terms/src/main/scala/Main.scala
@@ -1,46 +1,55 @@
 import scala.runtime.stdLibPatches.language.experimental.namedTypeArguments
+
 @main
-def hello: Unit = 
-  // val t = Apl("x", λ("x", "y"))
-  // val t2 = λ("y", Apl("x", λ("x", "y")))
-  // println(t)
-  // val t3 = currySub(t, "y", t2)
-  // println(t2)
-  // val t4 = currySub(t3, "x", t3)
-  // println(NewVar(t, t2))
-  
-  // val (term, rest) = named.parseString("(λy(x(λxy)))")
-  // println(term)
+def main(): Unit = 
 
-  // (y(λx xyz)z)[y -> zy]
+  println("((y(λx((xy)z)))z)[y -> (zy)]")
   println(named.currySub(named.parseString("((y(λx((xy)z)))z)"), 'y', named.parseString("(zy)")))
-  // y(λx xy(λuuxy))[y -> z(λv vyz)]
+  println()
+ 
+  println("(y(λx((xy)(λu((ux)y)))))[y -> z(λv vyz)")
   println(named.currySub(named.parseString("(y(λx((xy)(λu((ux)y)))))"), 'y', named.parseString("(z(λv((vy)z)))")))
-  // 0(λ01(λ012))[0 -> 2(λ013)]
-  println(nameless.sub(nameless.Apl(0, nameless.λ(nameless.Apl(nameless.Apl(0,1), nameless.λ(nameless.Apl(nameless.Apl(0, 1), 2))))), 0, nameless.Apl(2, nameless.λ(nameless.Apl(nameless.Apl(0, 1), 3)))))
+  println()
+
+  println("0(λ01(λ012))[0 -> 2(λ013)]")
   println(nameless.sub(nameless.parseString("(0(λ((01)(λ((01)2)))))"), 0, nameless.parseString("(2(λ((01)3)))")))
+  println()
 
+  println("(λx((xy)((λu((zu)(λv(vy))))z))) to nameless")
   println(deleteNames(named.parseString("(λx((xy)((λu((zu)(λv(vy))))z)))"), Vector(named.Var('y'), named.Var('x'), named.Var('z'))))
-  println(addNames(nameless.parseString("(0(λ((01)(λ((01)2)))))"), Vector(named.Var('x'), named.Var('y'))))
+  println()
 
+  println("(0(λ((01)(λ((01)2))))) to named")
+  println(addNames(nameless.parseString("(0(λ((01)(λ((01)2)))))"), Vector(named.Var('x'), named.Var('y'))))
+  println()
+
+  println("(0(λ((01)(λ((01)2))))) -> named -> nameless")
   val withNames = addNames(nameless.parseString("(0(λ((01)(λ((01)2)))))"), Vector(named.Var('x'), named.Var('y')))
   println(deleteNames(withNames, Vector(named.Var('x'), named.Var('y'))))
+  println()
 
+  println("(λx((xy)((λu((zu)(λv(vy))))z))) -> nameless -> named")
   val withoutNames = deleteNames(named.parseString("(λx((xy)((λu((zu)(λv(vy))))z)))"), Vector(named.Var('y'), named.Var('x'), named.Var('z')))
   println(addNames(withoutNames, Vector(named.Var('y'), named.Var('x'), named.Var('z'))))
+  println()
 
+  // sub of nameless should be equal to nameless -> named -> curry sub -> nameless
   val m = nameless.parseString("(0(λ((01)(λ((01)2)))))")
   val x = 0
-  val n = nameless.parseString("(2(λ((01)3)))")  
+  val n = nameless.parseString("(2(λ((01)3)))")
   val mSubn = nameless.sub(m, x, n)
-
+  
   val m1 = addNames(m, Vector(named.Var('y'), named.Var('x'), named.Var('z')))
   val n1 = addNames(n, Vector(named.Var('y'), named.Var('x'), named.Var('z')))
   val m1Subn1 = named.currySub(m1, 'y', n1)
   
+  println("(0(λ((01)(λ((01)2)))))[0 -> (2(λ((01)3)))]")
   println(mSubn)
+  println()
+
+  println("but through: add names -> curry sub -> delete names")
   println(deleteNames(m1Subn1, Vector(named.Var('y'), named.Var('x'), named.Var('z'))))
+  println()
 
-  println(deleteNames(named.parseString("(y(λx((xy)(λu((ux)y)))))"), Vector(named.Var('y'), named.Var('x'), named.Var('z'))))
-
+  println("(λy(λx(xy))) and (λx(λy(yx))) should be alpha equivalent")
   println(named.areAlphaEq(named.parseString("(λy(λx(xy)))"), named.parseString("(λx(λy(yx)))")))
