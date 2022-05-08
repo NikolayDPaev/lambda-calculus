ec9ad4edf9d421569ea03e9ebcc7d51972402150
NikolayDPaev <niki.paev@gmail.com>
2022-05-08 19:38:48 +0300

    added some assertions in the parse method

--- lambda-terms/src/main/scala/named/Parse.scala
+++ lambda-terms/src/main/scala/named/Parse.scala
@@ -3,7 +3,8 @@ package named
 // string must be in this grammar:
 // L = x | (LL) | (λxL)
 // where x is single character
-def parseStringHelper(str: String): (Term, String) = 
+def parseStringHelper(str: String): (Term, String) =
+  assert(!str.isEmpty)
   if str.head != '(' then (Var(str.head), str.tail)
   else
     val token = str.tail
@@ -11,13 +12,13 @@ def parseStringHelper(str: String): (Term, String) =
       case 'λ' =>
         val x = token.tail.head
         val (p, rest) = parseStringHelper(token.tail.tail)
-        assert(rest.head == ')')
+        assert(!rest.isEmpty && rest.head == ')')
         val rest2 = rest.tail
         (λ(Var(x),p), rest2)
       case _ =>
         val (m, rest) = parseStringHelper(token)
         val (n, rest2) = parseStringHelper(rest)
-        assert(rest2.head == ')')
+        assert(!rest2.isEmpty && rest2.head == ')')
         val rest3 = rest2.tail
         (Apl(m, n), rest3)   
           
